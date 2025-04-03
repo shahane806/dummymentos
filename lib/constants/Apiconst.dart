13b8345c20b719586d9296d13eeb9a2be9b83a18
Om Shahane <71377782+shahane806@users.noreply.github.com>
2025-04-03 09:25:22 +0530

    Update Apiconst.dart

--- lib/constants/Apiconst.dart
+++ lib/constants/Apiconst.dart
@@ -1,4 +1,4 @@
 class Apiconst {
-  // static String Uri = "http://192.168.1.79:8500/";
-  static String Uri = "https://scanning.hmtcampus360v2.net/API_docsmangement/";
+  // static String Uri = "LOCAL BASE URL";
+  static String Uri = "BASE URL";
 }
